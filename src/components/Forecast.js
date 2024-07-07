90d672f1a3dbb06ad71089efae0f0ae1f82965fd
desai-pratik <pratikdesai2274@gmail.com>
2024-07-07 21:56:14 +0530

    timeline changes in card results.

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -9,7 +9,7 @@ const Forecast = ({ forecast }) => {
           <Grid item key={index} xs={12} sm={6} md={4} lg={3}>
             <Card>
               <CardContent>
-                <Typography variant="body2">{new Date(day.dt_txt).toDateString()}</Typography>
+                <Typography variant="body2">{day.dt_txt}</Typography>
                 <Typography variant="body2">Temperature: {day.main.temp} °C</Typography>
                 <Typography variant="body2">Condition: {day.weather[0].description}</Typography>
               </CardContent>
